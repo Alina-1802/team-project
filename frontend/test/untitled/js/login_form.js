7b763526fdc8aee6565cd8769fe128563affbd2b
Turquw <95191951+AliceAlex06@users.noreply.github.com>
2024-12-20 20:12:35 +0100

    Doddanie templatki/konczenie logowania
    Została dodana templatka główna do strony oraz zostało  udoskonalone logowanie

--- frontend/test/untitled/js/login_form.js
+++ frontend/test/untitled/js/login_form.js
@@ -1,7 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('change_panel_register').addEventListener('click', function() {
-        document.querySelectorAll('.login').forEach(element => {
+        document.querySelectorAll('.panel_login_register').forEach(element => {
             element.style.animation = 'move-right 0.3s linear forwards';
         });
+        document.querySelector('.login').classList.toggle("active");
+        document.querySelector('.register').classList.toggle("active");
+
+    });
+    document.getElementById('forgot_password').addEventListener('click', function() {
+        document.querySelectorAll('.panel_login_register').forEach(element => {
+            element.style.animation = 'move-right 0.3s linear forwards';
+        });
+        document.querySelector('.login').classList.toggle("active");
+        document.querySelector('.forgot_password').classList.toggle("active");
+
+    });
+    document.getElementById('change_panel_register_back').addEventListener('click', function() {
+        document.querySelectorAll('.panel_login_register').forEach(element => {
+            element.style.animation = 'move-left 0.3s linear forwards';
+        });
+        document.querySelector('.login').classList.toggle("active");
+        document.querySelector('.register').classList.remove("active");
+        document.querySelector('.forgot_password').classList.remove("active");
+
     });
 });
