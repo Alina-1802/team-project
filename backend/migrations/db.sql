19426afaf0b0dbb18dc8ce82e5a4d981a5a68322
KuroYwain <bartek4488@interia.pl>
2025-01-29 18:15:00 +0100

    get user

--- backend/migrations/db.sql
+++ backend/migrations/db.sql
@@ -1,5 +1,20 @@
+
+DROP TABLE users;
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    first_name VARCHAR(255) DEFAULT NULL,
+    last_name VARCHAR(255) DEFAULT NULL,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS quiz_result (
+    quiz_result_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    quiz_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    scored_points INTEGER NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
 );
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,22 +1,28 @@
+mod auth;
+
 use std::sync::Arc;
-use axum::{
-    extract::{Path, State, Request},
-    http::StatusCode,
-    routing::{get, patch},
-    Json, Router, Extension,
-};
+use std::usize::MAX;
+use axum::{extract::{FromRequestParts, Path, State, Request}, http::StatusCode, routing::{get, patch}, Json, Router, Extension, middleware};
 use axum::routing::{connect, post, Route};
 use serde::{Serialize, Deserialize};
-use serde_json::json;
+use serde_json::{json, to_string, Value};
 use argon2::{Argon2, password_hash::{SaltString, PasswordHasher, PasswordVerifier}, PasswordHash};
 use async_trait::async_trait;
 use dotenvy::dotenv;
-use jsonwebtoken::{encode, decode, EncodingKey, DecodingKey, Header, Validation};
+use jsonwebtoken::{encode, decode, EncodingKey, DecodingKey, Header, Validation, Algorithm};
 use sqlx::{query, sqlite::{SqlitePoolOptions, SqlitePool}};
 use tokio::net::TcpListener;
 use axum::{response::IntoResponse};
+use axum::body::Body;
 use axum::extract::OptionalFromRequest;
+use axum::http::header;
+use axum::http::request::Parts;
+use axum::middleware::Next;
+use axum::response::Response;
+use headers::{Authorization, HeaderMapExt};
+use headers::authorization::Bearer;
 use validator::{Validate, ValidationErrors};
+use axum_extra::TypedHeader;
 
 
 
@@ -38,17 +44,20 @@ async fn main() {
     let app = Router::new()
         .route("/register", post(register_user))
         .route("/login", post(login_user))
+        // .route("/update-account", patch(update_user::<Body>)/*.route_layer(middleware::from_fn(auth_middleware::<Body>))*/)
+        .route("/get-user", get(get_user).route_layer(middleware::from_fn(auth_middleware::<Body>)))
         .layer(Extension(Arc::new(db_pool)));
+
 
     axum::serve(listener,app).await.expect("Error serving application")
 }
 
 #[derive(Deserialize,Validate)]
 struct RegisterRequest {
-    #[validate(length(min = 3, max = 30, message = "Username must be between 3 and 30 characters"))]
-    username: String,
+    #[validate(email(message = "Invalid email format"))]
+    #[validate(length(min = 10, max = 50, message = "Email must be between 10 and 50 characters"))]
+    email: String,
     #[validate(length(min = 8, message = "Password must be at least 8 characters long"))]
-    // #[validate(regex = "r#\"^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$\"#", message = "Password must contain at least one letter and one number")]
     password: String,
 }
 
@@ -56,16 +65,50 @@ struct RegisterRequest {
 struct ApiResponse {
     success: bool,
     message: String,
+    data: Vec<Value>
+}
+#[derive(Deserialize,Validate)]
+struct LoginRequest {
+    email: String,
+    password: String,
 }
 
-async fn register_user(
+#[derive(Serialize)]
+struct LoginResponse {
+    token: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+  pub sub: String,
+  pub exp: usize,
+}
+struct AuthenticatedUser {
+    pub id: String,
+}
+#[derive(Debug, Serialize, Deserialize)]
+struct UpdateUserRequest {
+    pub username: Option<String>,
+    pub first_name: Option<String>,
+    pub last_name: Option<String>,
+}
+#[derive(Debug, Serialize, Deserialize)]
+struct GetUserDataResponse {
+    pub username: String,
+    pub first_name: String,
+    pub last_name: String,
+    pub email: String,
+}
+
+async fn register_user (
     Extension(pool): Extension<Arc<SqlitePool>>,
     Json(payload): Json<RegisterRequest>,
-) -> impl IntoResponse {
+) -> impl IntoResponse  {
     if let Err(error) = validate_register_request(&payload) {
-        let response = ApiResponse {
+        let response : ApiResponse = ApiResponse {
             success: false,
             message: format!("Validation error: {}", error),
+            data: Vec::new(),
         };
         return (StatusCode::UNPROCESSABLE_ENTITY, Json(response));
     }
@@ -77,8 +120,8 @@ async fn register_user(
         .to_string();
 
     let result = query!(
-        "INSERT INTO users (username, password_hash) VALUES (?, ?)",
-        payload.username,
+        "INSERT INTO users (email, password_hash) VALUES (?, ?)",
+        payload.email,
         password_hash
     )
         .execute(&*pool)
@@ -89,6 +132,7 @@ async fn register_user(
             let response = ApiResponse {
                 success: true,
                 message: "User registered successfully".to_string(),
+                data: Vec::new(),
             };
             (StatusCode::CREATED, Json(response))
         }
@@ -96,30 +140,20 @@ async fn register_user(
             let response = ApiResponse {
                 success: false,
                 message: format!("Failed to register user: {:?}", err),
+                data: Vec::new(),
             };
             (StatusCode::INTERNAL_SERVER_ERROR, Json(response))
         }
     }
 }
 
-#[derive(Deserialize,Validate)]
-struct LoginRequest {
-    username: String,
-    password: String,
-}
-
-#[derive(Serialize)]
-struct LoginResponse {
-    token: String,
-}
-
-async fn login_user(
+pub async fn login_user(
     Extension(pool): Extension<Arc<SqlitePool>>,
     Json(payload): Json<LoginRequest>,
 ) -> Result<Json<LoginResponse>, Json<String>> {
     let user = query!(
-        "SELECT password_hash FROM users WHERE username = ?",
-        payload.username
+        "SELECT password_hash FROM users WHERE email = ?",
+        payload.email
     )
         .fetch_one(&*pool)
         .await;
@@ -131,7 +165,8 @@ async fn login_user(
                 .verify_password(payload.password.as_bytes(), &parsed_hash)
                 .is_ok()
             {
-                let claims = serde_json::json!({ "sub": payload.username });
+                let claims = Claims { sub: payload.email, exp: usize::MAX};
+                let claims = serde_json::json!(claims);
                 let token = encode(
                     &Header::default(),
                     &claims,
@@ -147,15 +182,8 @@ async fn login_user(
         Err(_) => Err(Json("Invalid username or password".into())),
     }
 }
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Claims {
-    sub: String,
-    exp: usize,
-}
-
-fn validate_register_request(req: &RegisterRequest) -> Result<(), String> {
-    if req.username.len() < 3 {
+pub fn validate_register_request(req: &RegisterRequest) -> Result<(), String> {
+    if req.email.len() < 3 {
         return Err("Username must be at least 3 characters long".to_string());
     }
 
@@ -165,7 +193,6 @@ fn validate_register_request(req: &RegisterRequest) -> Result<(), String> {
 
     Ok(())
 }
-
 fn verify_token(token: &str) -> Result<Claims, jsonwebtoken::errors::Error> {
     decode::<Claims>(
         token,
@@ -173,4 +200,141 @@ fn verify_token(token: &str) -> Result<Claims, jsonwebtoken::errors::Error> {
         &Validation::default(),
     )
         .map(|data| data.claims)
+}
+pub async fn auth_middleware<B>(
+    mut req: Request<Body>,
+    next: Next
+) -> Result<Response, (StatusCode, &'static str)> {
+    let headers = req.headers();
+
+    let authorization = headers
+        .typed_get::<Authorization<headers::authorization::Bearer>>()
+        .ok_or((StatusCode::UNAUTHORIZED, "Missing Authorization header"))?;
+
+    let mut auth_data = authorization.token()
+        .split(" ")
+        .collect::<Vec<&str>>();
+    let token= auth_data.pop()
+        .ok_or( (StatusCode::UNAUTHORIZED, "Invalid or expired token"))?;
+
+
+    let decoding_key = DecodingKey::from_secret("your_secret_key".as_bytes());
+    let token_data = decode::<Claims>(
+        token,
+        &decoding_key,
+        &Validation::new(Algorithm::HS256),
+    ).map_err(|_| (StatusCode::UNAUTHORIZED, "Invalid or expired token123"))?;
+
+     req.extensions_mut().insert(token_data.claims.sub);
+
+    Ok(next.run(req).await)
+}
+pub async fn update_user<Body>(
+    Extension(pool): Extension<Arc<SqlitePool>>,
+    Json(payload): Json<UpdateUserRequest>,
+) -> impl IntoResponse {
+
+    if payload.first_name.is_none() && payload.last_name.is_none() && payload.username.is_none() {
+        let response = ApiResponse {
+            success: false,
+            message: "No fields to update".to_string(),
+            data: Vec::new(),
+        };
+        // return (StatusCode::BAD_REQUEST, Json(response));
+    }
+
+    let mut query_string = String::from("UPDATE users SET ");
+    let mut params: Vec<String> = Vec::new();
+
+    // if let Some(first_name) = &payload.first_name {
+    //     query_string.push_str("first_name = ?, ");
+    //     params.push(first_name.clone());
+    // }
+    //
+    // if let Some(last_name) = &payload.last_name {
+    //     query_string.push_str("last_name = ?, ");
+    //     params.push(last_name.clone());
+    // }
+    //
+    // if let Some(username) = &payload.username {
+    //     query_string.push_str("username = ?, ");
+    //     params.push(username.clone());
+    // }
+
+    query_string.pop();
+    query_string.pop();
+
+    query_string.push_str(" WHERE email = ?;");
+    let  email = "123".to_string()/*req.extensions().get::<String>().expect("REASON").to_string()*/;
+    params.push(email);
+
+
+    let result = sqlx::query(&query_string)
+        .bind(&params[0])
+        .bind(&params[1])
+        .bind(&params[2])
+        .bind(&params[3])
+        .execute(&*pool)
+        .await;
+
+    match result {
+        Ok(_) => {
+            let response : ApiResponse = ApiResponse {
+                success: true,
+                message: "User updated successfully".to_string(),
+                data: Vec::new(),
+            };
+            (StatusCode::OK, Json(response))
+        }
+        Err(err) => {
+            let response = ApiResponse {
+                success: false,
+                message: format!("Failed to update user: {:?}", err),
+                data: Vec::new(),
+            };
+            (StatusCode::INTERNAL_SERVER_ERROR, Json(response))
+        }
+    }
+}
+pub async fn get_user(
+    Extension(pool): Extension<Arc<SqlitePool>>,
+    TypedHeader(authorization): TypedHeader<Authorization<headers::authorization::Bearer>>,
+    mut req: Request<Body>,
+) -> impl IntoResponse {
+    let email = req.extensions().get::<String>();
+
+    let user = query!(
+        "SELECT username,first_name,last_name,email FROM users WHERE email = ?",
+        email
+    )
+        .fetch_one(&*pool)
+        .await;
+
+    match user {
+        Ok(user) => {
+            let user_data: Vec<Value> = vec![
+                serde_json::json!({
+                    "username": user.username,
+                    "first_name": user.first_name,
+                    "last_name": user.last_name,
+                    "email": user.email
+                })
+            ];
+            let response = ApiResponse {
+                success: true,
+                message: "User found".to_string(),
+                data: user_data,
+            };
+            (StatusCode::OK, Json(response))
+
+        }
+        Err(err) => {
+            let response = ApiResponse {
+                success: false,
+                message: format!("User not found: {:?}", err),
+                data: Vec::new(),
+            };
+            (StatusCode::INTERNAL_SERVER_ERROR, Json(response))
+        }
+    }
 }
